d4c05a5d6a9945c1e6b2670c94d275f868ae1fb7
MotionBlue <ape.7107@outlook.com>
2021-04-12 16:21:36 +0600

    signupActivity Design

--- app/src/main/java/com/example/examstation/MainActivity.java
+++ app/src/main/java/com/example/examstation/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.examstation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button signUP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signUP = findViewById(R.id.signupBtn);
+        signUP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
